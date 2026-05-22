7707cd6cae2fb797fe32a47f87a6eefc49bc442b
trgiahuy14 <trgiahuy14@gmail.com>
2026-05-22 17:35:03 +0700

    feat: integrate Reverb and Pusher broadcasting

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -6,6 +6,8 @@ import select2 from 'select2';
 select2();
 
 import 'select2/dist/css/select2.min.css';
+import Echo from 'laravel-echo';
+import Pusher from 'pusher-js';
 
 window.$ = window.jQuery = $;
 
@@ -20,4 +22,16 @@ window.$.ajaxSetup({
     headers: {
         'X-CSRF-TOKEN': csrfToken,
     },
+});
+
+window.Pusher = Pusher;
+
+window.Echo = new Echo({
+    broadcaster: 'reverb',
+    key: import.meta.env.VITE_REVERB_APP_KEY,
+    wsHost: import.meta.env.VITE_REVERB_HOST,
+    wsPort: import.meta.env.VITE_REVERB_PORT ?? 80,
+    wssPort: import.meta.env.VITE_REVERB_PORT ?? 443,
+    forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'https') === 'https',
+    enabledTransports: ['ws', 'wss'],
 });
