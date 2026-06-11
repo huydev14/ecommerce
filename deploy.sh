ca504b5ffe3f501eaf1c8d169b6514a997aa96ec
trgiahuy14 <trgiahuy14@gmail.com>
2026-06-11 10:11:31 +0700

    ci: update production ci/cd scripts

--- deploy.sh
+++ deploy.sh
@@ -2,27 +2,22 @@
 
 set -e
 
-echo ">>> Pulling latest code from Git..."
-git pull origin main
 
-echo ">>> Restarting Docker containers..."
-docker compose down
-docker compose up -d --build
 
-echo ">>> Running database migrations..."
-docker compose exec -T web php artisan migrate --force
+echo ">>> Running database migrations"
+ecommerce php artisan migrate --force
 
-echo ">>> Optimizing Laravel Cache..."
-docker compose exec -T web php artisan optimize
-docker compose exec -T web php artisan view:cache
+echo ">>> Clearing cache"
+ecommerce php artisan optimize
+ecommerce php artisan view:cache
 
 echo ">>> Restarting Queue..."
 
-docker compose exec -T web php artisan queue:restart
+ecommerce php artisan queue:restart
 
-if docker compose exec -T web php artisan list | grep -q 'horizon:terminate'; then
+if ecommerce php artisan list | grep -q 'horizon:terminate'; then
     echo ">>> Restarting Laravel Horizon..."
-    docker compose exec -T web php artisan horizon:terminate
+    ecommerce php artisan horizon:terminate
 fi
 
-echo "=== DEPLOYMENT SUCCESSFUL ==="
+echo ">>> DEPLOYMENT SUCCESSFUL"
