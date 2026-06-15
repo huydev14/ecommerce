8fad63668193012cbeed640ac209a493898d0f21
trgiahuy14 <trgiahuy14@gmail.com>
2026-06-15 10:11:33 +0700

    ci: update deploy scripts

--- deploy.sh
+++ deploy.sh
@@ -1,24 +1,19 @@
 #!/bin/bash
 set -e
 
-artisan() {
-    cd /srv/web-host
-    docker compose exec -T php84 sh -lc "cd /var/www/html/app84/ecommerce && php artisan $*"
-}
+echo "---- Running migrations ----"
+php artisan migrate --force
 
-echo ">>> Running migrations"
-artisan migrate --force
+echo "---- Clearing cache ----"
+php artisan optimize
+php artisan view:cache
 
-echo ">>> Clearing cache"
-artisan optimize
-artisan view:cache
+echo "---- Restarting queue ----"
+php artisan queue:restart
 
-echo ">>> Restarting queue"
-artisan queue:restart
-
-echo ">>> Restarting Laravel Horizon (nếu có)..."
-if artisan list | grep -q 'horizon:terminate' || true; then
-    artisan horizon:terminate || true
+echo "---- Restarting Laravel Horizon ----"
+if grep -q 'horizon:terminate' || true; then
+    php artisan horizon:terminate || true
 fi
 
-echo ">>> DEPLOYMENT SUCCESSFUL"
+echo "---- DEPLOYMENT SUCCESSFUL ----"
